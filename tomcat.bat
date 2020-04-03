0b6c6d181e96e85c1e8ee38710745768c6ba0c67
madhurichittabathina <54107842+madhurichittabathina@users.noreply.github.com>
2020-04-03 13:38:55 +0530

    Update tomcat.bat

--- tomcat.bat
+++ tomcat.bat
@@ -1,2 +1,2 @@
-del "C:\Program Files\Apache Software Foundation\Tomcat 8.5\webapps\cangkitsolutions.war"
-xcopy "C:\Program Files (x86)\Jenkins\workspace\madhuapp_master\target\cangkitsolutions.war" "C:\Program Files\Apache Software Foundation\Tomcat 8.5\webapps"
+del "C:\Program Files\Apache Software Foundation\Tomcat 9\webapps\cangkitsolutions.war"
+xcopy "C:\Program Files (x86)\Jenkins\workspace\madhuapp_master\target\cangkitsolutions.war" "C:\Program Files\Apache Software Foundation\Tomcat 9\webapps"
